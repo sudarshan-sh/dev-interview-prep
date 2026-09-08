70e10b349d0dd7a37f86b0f24c86b6c1d8a1411a
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-08 19:29:05 +0530

    fixed incorrect questions

--- 02_PostgreSQL/assignments/08-wildcards.sql
+++ 02_PostgreSQL/assignments/08-wildcards.sql
@@ -18,7 +18,7 @@ SELECT * from employee WHERE name ILIKE '%sh%';
 SELECT * from employee WHERE name ILIKE '%sh';
 
 -- 3. Select all employee records from Employee table where name has exactly two letters before 'n'.
-SELECT * from employee WHERE name ILIKE '%__n%';
+SELECT * from employee WHERE name ILIKE '__n%';
 
 -- 4. Select all employee records from Employee table where name has exactly 5 letters.
 SELECT * from employee WHERE name LIKE '_____';
@@ -27,4 +27,4 @@ SELECT * from employee WHERE name LIKE '_____';
 SELECT * from employee WHERE LENGTH(name)=5;
 
 -- 5. Select all the records from Employee table where names having first letter either A or N.
-SELECT * from employee WHERE name ILIKE 'a%' OR 'n%';
+SELECT * from employee WHERE name ILIKE 'a%' OR name ILIKE 'n%';
