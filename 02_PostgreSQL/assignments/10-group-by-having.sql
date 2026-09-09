c40749a4eb408f7dc28d36c3f2caae5afa536a05
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-09 20:58:59 +0530

    problems on group by & having

--- 02_PostgreSQL/assignments/10-group-by-having.sql
+++ 02_PostgreSQL/assignments/10-group-by-having.sql
@@ -25,3 +25,22 @@ FROM courses c
 INNER JOIN batches b ON b.courseid = c.courseid
 GROUP BY c.courseid, c.coursename
 HAVING COUNT(b.batchid) > 1;
+
+-- 3. What is the total number of student enrollments course-wise?
+SELECT COUNT(sb.batchid), c.coursename from students s
+INNER JOIN student_batches sb ON sb.rollno = s.rollno
+INNER JOIN batches b ON b.batchid = sb.batchid
+INNER JOIN courses c ON c.courseid = b.courseid
+GROUP BY c.coursename;
+
+-- 4. What is the total student seating capacity offered course-wise?
+SELECT c.coursename, SUM(b.size) AS total_capacity FROM batches b
+INNER JOIN courses c ON c.courseid = b.courseid
+GROUP BY c.coursename;
+
+-- 5. Which courses are highly popular and have generated more than 2 enrollments?
+SELECT c.coursename, COUNT(sb.rollno) AS total_enrolments from student_batches sb
+INNER JOIN batches b ON b.batchid = sb.batchid
+INNER JOIN courses c ON c.courseid = b.courseid
+GROUP BY c.coursename
+HAVING COUNT(sb.rollno) > 2;
